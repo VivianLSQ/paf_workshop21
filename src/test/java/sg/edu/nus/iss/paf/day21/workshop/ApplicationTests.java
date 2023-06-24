6073f76a395e8dbd522fef9266ba4d4e40dab604
VivianLim <vivianlimsq@gmail.com>
2023-06-24 16:23:49 +0800

    first commit

---
+++ src/test/java/sg/edu/nus/iss/paf/day21/workshop/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf.day21.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
